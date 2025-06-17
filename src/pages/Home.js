783bad2d2cd136c2ef32e122900d6d2cc1d4fa53
Savanth <savanth.jaggannagari6@gmail.com>
2025-06-17 13:46:20 -0500

    Initial commit for SkyToad Inc

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <section className="hero">
+        <h1>Welcome to SkyToad Inc.</h1>
+        <p>Your Partner in Growth & Strategy</p>
+        <a href="/services"><button>Explore Services</button></a>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
